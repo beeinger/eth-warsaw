73be444b46b685a035e9c87fd7e9abc4e9b05f3d
beeinger <sulecki.maciek@gmail.com>
2022-09-02 20:51:17 +0200

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-export default function index({}: Props) {
+export default function index() {
   function play() {
     var audio = new Audio("100101001011010101001010010100101");
     audio.play();
