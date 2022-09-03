48a464fbde733044a2861a283ce9e87345616d28
beeinger <sulecki.maciek@gmail.com>
2022-09-03 16:06:33 +0200

    quick mobile fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
-
+import Head from "next/head";
+import createCache from "@emotion/cache";
 import { globalStyles } from "../shared/styles";
 
 const cache = createCache({ key: "next" });
 
 const App = ({ Component, pageProps }) => (
   <CacheProvider value={cache}>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=0.75" />
+    </Head>
     {globalStyles}
     <Component {...pageProps} />
   </CacheProvider>
