d8b224854f10d91324cf015fc283aa5c9ccf3d96
Marcello Bardus <marcello@bardus.it>
2022-09-03 20:19:16 +0200

    init

--- contracts/contracts/starknet/L2Beat.cairo
+++ contracts/contracts/starknet/L2Beat.cairo
@@ -1,4 +1,29 @@
 %lang starknet
 %builtins pedersen range_check ecdsa bitwise
 
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+from starkware.cairo.common.cairo_builtins import BitwiseBuiltin
 
+@storage_var
+func _token_address() -> (res: felt):
+end
+
+@constructor
+func constructor{
+    syscall_ptr : felt*,
+    pedersen_ptr : HashBuiltin*,
+    range_check_ptr,
+}(token_address: felt):
+    _token_address.write(token_address)
+    return ()
+end
+
+@external
+func mint{
+        syscall_ptr : felt*,
+        pedersen_ptr : HashBuiltin*,
+        range_check_ptr,
+        bitwise_ptr : BitwiseBuiltin*
+    }(block_number: felt, txns_hashes_len: felt, txns_hashes: felt*):
+    return ()
+end
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,7 +3,25 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@shardlabs/starknet-hardhat-plugin";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  starknet: {
+    dockerizedVersion: "0.9.1",
+  },
+  networks: {
+    integratedDevnet: {
+      url: "http://127.0.0.1:5050",
+
+      // or specify Docker image tag
+      dockerizedVersion: "0.2.3-arm",
+
+      // optional devnet CLI arguments
+      args: ["--lite-mode", "--gas-price", "2000000000"],
+
+      // stdout: "logs/stdout.log" <- dumps stdout to the file
+      stdout: "STDOUT", // <- logs stdout to the terminal
+      // stderr: "logs/stderr.log" <- dumps stderr to the file
+      stderr: "STDERR", // <- logs stderr to the terminal
+    },
+  },
 };
 
 export default config;
